2ba8bc15067f92d46ec2c76b77abd2c6b83e387d
CharlesCatYT <62410083+CharlesCatYT@users.noreply.github.com>
2024-06-25 15:19:47 -0400

    Revert "framerate cap option"
    This reverts commit 820829ecdb0fca8c0656524c79c970cbda2c7d08.

--- source/funkin/save/Save.hx
+++ source/funkin/save/Save.hx
@@ -89,7 +89,6 @@ class Save
       options:
         {
           // Reasonable defaults.
-          framerate: 60,
           naughtyness: true,
           downscroll: false,
           flashingLights: true,
@@ -1056,12 +1055,6 @@ typedef SaveScoreTallyData =
  */
 typedef SaveDataOptions =
 {
-  /**
-   * FPS
-   * @default `60`
-   */
-  var framerate:Int;
-
   /**
    * Whether some particularly fowl language is displayed.
    * @default `true`
